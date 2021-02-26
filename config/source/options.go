66939e76caff5d8c08e77d3dec0082f3c979bb7e
slyao <slyao@tencent.com>
2021-02-26 16:33:42 +0800

    delete no necessary code

--- config/source/options.go
+++ config/source/options.go
@@ -3,7 +3,6 @@ package source
 import (
 	"context"
 
-	"github.com/micro/go-micro/v2/client"
 	"github.com/awatercolorpen/nitro/config/encoder"
 	"github.com/awatercolorpen/nitro/config/encoder/json"
 )
@@ -14,9 +13,6 @@ type Options struct {
 
 	// for alternative data
 	Context context.Context
-
-	// Client to use for RPC
-	Client client.Client
 }
 
 type Option func(o *Options)
@@ -25,7 +21,6 @@ func NewOptions(opts ...Option) Options {
 	options := Options{
 		Encoder: json.NewEncoder(),
 		Context: context.Background(),
-		Client:  client.DefaultClient,
 	}
 
 	for _, o := range opts {
@@ -39,12 +34,5 @@ func NewOptions(opts ...Option) Options {
 func WithEncoder(e encoder.Encoder) Option {
 	return func(o *Options) {
 		o.Encoder = e
-	}
-}
-
-// WithClient sets the source client
-func WithClient(c client.Client) Option {
-	return func(o *Options) {
-		o.Client = c
 	}
 }
